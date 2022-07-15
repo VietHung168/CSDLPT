b2e1e5933987042d02337af79727f02c66d39c3f
thienlydochanh <52105481+thienlydochanh@users.noreply.github.com>
2022-07-15 07:30:11 +0700

    4 commit

--- QLVT/FormLogin.cs
+++ QLVT/FormLogin.cs
@@ -16,5 +16,10 @@ namespace QLVT
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //comit
+        }
     }
 }
